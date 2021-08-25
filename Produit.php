05c180d6c0118b4a44ad3283565c8f1404f4abcf
Hasina1234 <89484785+Hasina1234@users.noreply.github.com>
2021-08-25 04:25:31 +0100

    Add files via upload

--- Produit.php
+++ Produit.php
@@ -2,6 +2,7 @@
 
 class Produit extends CI_Model{
 	
+	
 	public function getAllProduit(){
 
 		$sql = 'SELECT * FROM Produit';
@@ -11,6 +12,21 @@ class Produit extends CI_Model{
 			$produit[] = $key;
 		}
 		return $produit;
+	}
+	public function insertProduit($idProduit,$idCategorie,$nomProduit,$prixUnitaire){
+		$sql = "INSERT INTO Produit VALUES (%s,%s,%s,%s)";
+		$sql = sprintf($sql,$this->db->escape($idProduit),$this->db->escape($idCategorie),$this->db->escape($nomProduit),$this->db->escape($prixUnitaire));
+		$this->db->query($sql);
+	}
+	public function updateProduit($idProduit,$idCategorie,$nomProduit,$prixUnitaire){
+		$sql = "UPDATE Produit set idCategorie='%s',nomProduit='%s',prixUnitaire='%s' where idProduit='%s'";
+		$sql = sprintf($sql,$this->db->escape($idCategorie),$this->db->escape($nomProduit),$this->db->escape($prixUnitaire),$this->db->escape($idProduit));
+		$this->db->query($sql);
+	}
+	public function deleteProduit($idProduit){
+		$sql = "DELETE FROM Produit where idProduit='%s'";
+		$sql = sprintf($sql,$this->db->escape($idProduit));
+		$this->db->query($sql);
 	}
 }
 ?>
